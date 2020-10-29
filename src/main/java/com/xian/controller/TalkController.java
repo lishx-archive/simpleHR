4164a81a8a9462d6beb7075f5d5ac1c22d5e773f
lishouxian <li.shouxian@outlook.com>
2020-10-29 19:34:01 +0800

    聊天UI测试完成2

--- src/main/java/com/xian/controller/TalkController.java
+++ src/main/java/com/xian/controller/TalkController.java
@@ -5,6 +5,7 @@ import com.xian.entities.EmployeeEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -14,8 +15,10 @@ import java.util.Map;
 public class TalkController {
 
 
-    @GetMapping("/talk")
+    @PostMapping("/talk/message")
     public String listEmployee(){
+
+        
 
         return "talk/list";
     }
